89a01c8389528f6c1ad6f3ddd790de8ba22aba31
willian <williandeitosi@gmail.com>
2024-11-05 11:45:02 -0300

    feat: upgrade the bot

--- main.py
+++ main.py
@@ -15,30 +15,29 @@ linkedin_password = os.getenv("LINKEDIN_PASSWORD")
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-access = [linkedin_email, linkedin_password, "recruiter AND hiring AND python"]
+access = [linkedin_email, linkedin_password, "backend"]
 driver.get("https://www.linkedin.com")
-sleep(3)
-
-email = driver.find_element(By.XPATH, '//input[@id="session_key"]')
+sleep(5)
+email = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/a')
 email.click()
-email.send_keys(access[0])
+sleep(2)
+putEmail = driver.find_element(By.XPATH, '//*[@id="username"]')
+putEmail.send_keys(access[0])
 
-sleep(0.5)
 
-password = driver.find_element(By.XPATH, '//input[@id="session_password"]')
+sleep(1)
+password = driver.find_element(By.XPATH, '//*[@id="password"]')
 password.click()
 password.send_keys(access[1])
 
 sleep(0.5)
-
-enter = driver.find_element(By.XPATH, '//button[@data-id="sign-in-form__submit-btn"]')
+enter = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
 enter.click()
 
 sleep(5)
 
-pesquisar = driver.find_element(
-    By.XPATH, '//input[@class="search-global-typeahead__input"]'
-)
+# input("")
+pesquisar = driver.find_element(By.XPATH, '//*[@id="global-nav-typeahead"]/input')
 pesquisar.click()
 pesquisar.send_keys(access[2])
 pesquisar.send_keys(Keys.ENTER)
@@ -54,47 +53,56 @@ for btn in buttons:
         btn.click()
         sleep(5)
 
-        i=1
+        i = 1
         while True:
             try:
                 if i >= 10:
-                    i=1
-                    driver.execute_script('window.scroll(0, 9999)')
-                    avancar = driver.find_element(By.XPATH, '//*[@aria-label="Avançar"]')
+                    i = 1
+                    driver.execute_script("window.scroll(0, 9999)")
+                    avancar = driver.find_element(
+                        By.XPATH, '//*[@aria-label="Avançar"]'
+                    )
                     avancar.click()
-                conectar = driver.find_elements(By.XPATH,'//button[@class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"]',)
+                conectar = driver.find_elements(
+                    By.XPATH,
+                    '//button[@class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"]',
+                )
                 for conect in conectar:
-                    i+=1
+                    i += 1
                     if conect.text == "Conectar":
                         conect.click()
                         sleep(1)
                         nome_pessoa = driver.find_element(
-                            By.XPATH, '//div[@class="artdeco-modal__content ember-view"]'
+                            By.XPATH,
+                            '//div[@class="artdeco-modal__content ember-view"]',
                         )
                         strong = nome_pessoa.find_element(By.XPATH, ".//strong")
                         nome = strong.text
 
                         sleep(1)
-                        
+
                         adicionar_nota = driver.find_element(
-                            By.XPATH, '//button[@aria-label="Adicionar nota"]'
+                            By.XPATH, '//button[@aria-label="Enviar sem nota"]'
                         )
                         adicionar_nota.click()
 
-                        sleep(1)
+                        # sleep(1)
 
-                        mensagem = f"Olá {nome}, como vai? Estou buscando oportunidade na área de programação, estou usando um RPA para me comunicar com você que eu mesmo desenvolvi, adoraria fazer parte da sua rede. Podemos conversar?"
-                        textarea = driver.find_element(By.XPATH, '//textarea[@name="message"]')
-                        textarea.click()
-                        textarea.send_keys(mensagem)
+                        # mensagem = f"Olá {nome}, como vai? Estou buscando oportunidade na área de programação, estou usando um RPA para me comunicar com você que eu mesmo desenvolvi, adoraria fazer parte da sua rede. Podemos conversar?"
+                        # textarea = driver.find_element(
+                        #     By.XPATH, '//textarea[@name="message"]'
+                        # )
+                        # textarea.click()
+                        # textarea.send_keys(mensagem)
 
-                        sleep(1)
+                        # sleep(1)
 
-                        enviar = driver.find_element(
-                            By.XPATH, '//button[@aria-label="Enviar agora"]'
-                        )
-                        enviar.click()
+                        # enviar = driver.find_element(
+                        #     By.XPATH, '//button[@aria-label="Enviar agora"]'
+                        # )
+                        # enviar.click()
             except:
-                print('[ERRO]')
+                print("[ERRO]")
 
 driver.quit()
+# TODO: Melhorar o bot de conexoes, ele execulta normalmente , porem ainda da muitos erros e preciso de alguns if
